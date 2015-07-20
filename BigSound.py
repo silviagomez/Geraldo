b22ad72d3c5658844837176db24629bd2a98e662
Kevin Cole <dc.loco@gmail.com>
2015-07-20 18:02:42 -0400

    - Scaling up (pun intended). It now plays an ascending major scale.

--- BigSound.py
+++ BigSound.py
@@ -13,25 +13,60 @@ from pyb import *
 
 # The Passive buzzer (black, no sticker) takes a modulated sigal.
 
+# The chromatic scale is based on the 12th root of 2 = 1.0594630943592952645
+
+halfstep  = 1.0594630943592952645
+wholestep = halfstep * halfstep
+
+fundemental = 440.0
+pitch = {}
+pitch["C4"] = fundemental * wholestep * wholestep * halfstep
+pitch["D4"] = pitch["C4"] * wholestep
+pitch["E4"] = pitch["D4"] * wholestep
+pitch["F4"] = pitch["E4"] * halfstep
+pitch["G4"] = pitch["F4"] * wholestep
+pitch["A4"] = pitch["G4"] * wholestep
+pitch["B4"] = pitch["A4"] * wholestep
+pitch["C5"] = pitch["B4"] * halfstep
+
+scale = (pitch["C4"],
+         pitch["D4"],
+         pitch["E4"],
+         pitch["F4"],
+         pitch["G4"],
+         pitch["A4"],
+         pitch["B4"],
+         pitch["C5"])
+
 buzzer = Pin("X1", Pin.OUT_PP)  # Passive buzzer
 
 while True:
-    for i in range(80):  # 2ms period = 500 Hz
-        buzzer.high()
-        delay(1)
-        buzzer.low()
-        delay(1)
+    for note in scale:
+        for duration in range(100):
+            buzzer.high()
+            udelay(int(500000.0 / note))
+            buzzer.low()
+            udelay(int(500000.0 / note))
+        print(int(500000.0 / note))
+        delay(200)
 
-    for i in range(100):  # 4ms period = 250 Hz
-        buzzer.high()
-        delay(2)
-        buzzer.low()
-        delay(2)
+# while True:
+#     for i in range(80):  # 2ms period = 500 Hz
+#         buzzer.high()
+#         delay(1)
+#         buzzer.low()
+#         delay(1)
+#
+#     for i in range(100):  # 4ms period = 250 Hz
+#         buzzer.high()
+#         delay(2)
+#         buzzer.low()
+#         delay(2)
 
-DO = Pin("X5", Pin.IN)  # Read the Digital Output
-AO = ADC(Pin("X6"))     # Read the Analog Output
+# DO = Pin("X5", Pin.IN)  # Read the Digital Output
+# AO = ADC(Pin("X6"))     # Read the Analog Output
 
-#A0.read()
+# A0.read()
 
-#buffy = bytearray(10000)   # 10,000 samples...
-#AO.read_timed(buffy, 100)  # ...over 100 seconds (100 samples / second)
+# buffy = bytearray(10000)   # 10,000 samples...
+# AO.read_timed(buffy, 100)  # ...over 100 seconds (100 samples / second)
