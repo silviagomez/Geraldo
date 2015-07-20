c9bf822e5d830ca08895b93404d9fd616f00224b
GeorgeUng <ung.george.1@gmail.com>
2015-07-20 14:04:14 -0400

    Fixed some stylization issues.

--- LineFollower.py
+++ LineFollower.py
@@ -15,71 +15,71 @@ try:
 
 
     def stop():
-	    LED(1).on()
-	    LED(2).off()
-	    LED(3).off()
-	    red.low()
-	    blue.low()
-	    Rservo.speed(-2)
-	    Lservo.speed(-2)
+        LED(1).on()
+        LED(2).off()
+        LED(3).off()
+        red.low()
+        blue.low()
+        Rservo.speed(-2)
+        Lservo.speed(-2)
 
 
     def handleLights(LSvalue):
-	    if LSvalue == 1:
-		    LED(1).off()
-		    LED(2).on()
-		    LED(3).off()
-		    moveStraight()
-		    red.low()
-		    blue.high()
-	    else:
-		    LED(1).off()
-		    LED(3).on()
-		    LED(2).off()
-		    turnRight()
-		    blue.low()
-		    red.high()
+        if LSvalue == 1:
+            LED(1).off()
+            LED(2).on()
+            LED(3).off()
+            moveStraight()
+            red.low()
+            blue.high()
+        else:
+            LED(1).off()
+            LED(3).on()
+            LED(2).off()
+            turnRight()
+            blue.low()
+            red.high()
 
 
     def moveStraight():
-	    Rservo.speed(-50)
-	    Lservo.speed(38)
+        Rservo.speed(-50)
+        Lservo.speed(38)
 
-	
+
     def turnRight():
-	    Rservo.speed(-2)
-	    Lservo.speed(100)
+        Rservo.speed(-2)
+        Lservo.speed(100)
 
-	
+
     def turnLeft():
-	    Rservo.speed(-100)
-	    Lservo.speed(-2)
+        Rservo.speed(-100)
+        Lservo.speed(-2)
 
 
     def tracker():
-	    stop()
-	    buttonValue, currentState = 1, False
-	    while True:
-		    LSvalue = LS.value()
-		    newButtonValue = button.value()
-		    if LSvalue == 1 and currentState:
-			    handleLights(LSvalue)
-			    moveStraight()
-			    red.low()
-			    blue.high()
-			    #delay(100)
-		    elif currentState:
-			    handleLights(LSvalue)
-			    turnRight()
-			    blue.low()
-			    red.high()
-			    #delay(100)
+        stop()
+        buttonValue, currentState = 1, False
+        while True:
+            LSvalue = LS.value()
+            newButtonValue = button.value()
+            if LSvalue == 1 and currentState:
+                handleLights(LSvalue)
+                moveStraight()
+                red.low()
+                blue.high()
+                #delay(100)
+            elif currentState:
+                handleLights(LSvalue)
+                turnRight()
+                blue.low()
+                red.high()
+                #delay(100)
 
-		    if newButtonValue != buttonValue:
-			    buttonValue = newButtonValue
-			    currentState = not currentState if buttonValue == 0 else currentState
-			    if not currentState:
-				    stop()
+            if newButtonValue != buttonValue:
+                buttonValue = newButtonValue
+                currentState = not currentState if buttonValue == 0 else currentState
+                if not currentState:
+                    stop()
 
 
     tracker()
