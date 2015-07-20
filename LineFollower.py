a1525ad02ba2706b67ec6d5862e79c97572f0b4b
GeorgeUng <ung.george.1@gmail.com>
2015-07-20 13:55:57 -0400

    Changed the the actual current Geraldo

--- LineFollower.py
+++ LineFollower.py
@@ -1,70 +1,89 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+
+# main.py -- put your code here!
+
 try:
-	from pyb import DAC
-	from pyb import LED
-	from pyb import Pin
-	from pyb import USB_VCP
-	
-	sensitivity, maxn, minn = .5, 255, 0
-	
-	com = USB_VCP()
-	light = DAC(2)
-	y1, y2, y3 = Pin('Y1', Pin.IN), Pin('Y2', Pin.IN), Pin('Y3', Pin.IN)
-	intensity, oldStr = maxn, str(y1.value()) + str(y2.value())
-	
-	directTab = {
-	'1000' : 1,
-	'0001' : 1,
-	'0111' : 1,
-	'1110' : 1,
-	'1011' : -1,
-	'1101' : -1,
-	'0100' : -1,
-	'0010' : -1
-	}
-	
-	
-	def setByte(i):
-		return i.to_bytes(1)
+    from pyb import *
+
+    LS = Pin('X5', Pin.IN)
+    Lservo = Servo(2)
+    Rservo = Servo(1)
+    red = Pin('Y6', Pin.OUT_PP)
+    blue = Pin('Y4', Pin.OUT_PP)
+    button = Pin('Y1', Pin.IN)
+
+
+    def stop():
+	    LED(1).on()
+	    LED(2).off()
+	    LED(3).off()
+	    red.low()
+	    blue.low()
+	    Rservo.speed(-2)
+	    Lservo.speed(-2)
+
+
+    def handleLights(LSvalue):
+	    if LSvalue == 1:
+		    LED(1).off()
+		    LED(2).on()
+		    LED(3).off()
+		    moveStraight()
+		    red.low()
+		    blue.high()
+	    else:
+		    LED(1).off()
+		    LED(3).on()
+		    LED(2).off()
+		    turnRight()
+		    blue.low()
+		    red.high()
+
+
+    def moveStraight():
+	    Rservo.speed(-50)
+	    Lservo.speed(38)
 
 	
-	def limitNums(num, maxn, minn):
-		return max(minn, min(maxn, num))
+    def turnRight():
+	    Rservo.speed(-2)
+	    Lservo.speed(100)
 
-	def handleLEDs(intensity, value):
-		if intensity == maxn:
-			LED(4).on()
-			LED(1).off()
-		elif intensity == minn:
-			LED(1).on()
-			LED(4).off()
-		else:
-			LED(1).off()
-			LED(4).off()
-		
-		if value > 0 and intensity != maxn:
-			LED(2).on()
-			LED(3).off()
-		elif value < 0 and intensity != minn:
-			LED(3).on()
-			LED(2).off()
-		else:
-			LED(2).off()
-			LED(3).off()
+	
+    def turnLeft():
+	    Rservo.speed(-100)
+	    Lservo.speed(-2)
 
-	while True:
-		v1, v2 = y1.value(), y2.value()
-		currentStr = str(v1) + str(v2)
-		usedStr = oldStr + currentStr
-		oldStr = currentStr if directTab.get(usedStr) else oldStr
 
-		additive = 0 if not directTab.get(usedStr) else directTab[usedStr] * sensitivity * (maxn - minn)/255
-		intensity = limitNums(intensity + additive, maxn, minn)
-		light.write_timed(setByte(round(intensity)), 1)
+    def tracker():
+	    stop()
+	    buttonValue, currentState = 1, False
+	    while True:
+		    LSvalue = LS.value()
+		    newButtonValue = button.value()
+		    if LSvalue == 1 and currentState:
+			    handleLights(LSvalue)
+			    moveStraight()
+			    red.low()
+			    blue.high()
+			    #delay(100)
+		    elif currentState:
+			    handleLights(LSvalue)
+			    turnRight()
+			    blue.low()
+			    red.high()
+			    #delay(100)
 
-		print('Additive:', additive, 'Value:', intensity, eol = '\n')
-		handleLEDs(intensity, additive)
+		    if newButtonValue != buttonValue:
+			    buttonValue = newButtonValue
+			    currentState = not currentState if buttonValue == 0 else currentState
+			    if not currentState:
+				    stop()
 
-except BaseException as errStr:
-	f = open('error.txt', 'w')
-	f.write(str(errStr))
-	f.close()
+
+    tracker()
+except BaseException as errString:
+    f = open('errorLog.txt', 'w')
+    f.write(str(errString))
+    f.close()
